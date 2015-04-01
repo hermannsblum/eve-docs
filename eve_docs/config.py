e43cebb0af7c72287c49b7f7ebf6ec7e55ac0400
hermannsblum <hermannsblum@yahoo.de>
2015-04-01 15:56:14 +0200

    documentation

--- eve_docs/config.py
+++ eve_docs/config.py
@@ -2,10 +2,25 @@ from flask import current_app as capp
 from eve.utils import home_link
 from .labels import LABELS
 import re
-from pprint import pprint
 
 
 def get_cfg():
+    """
+    Will get all necessary data out of the eve-app.
+    It reads 'SERVER_NAME', 'API_NAME', 'BLUEPRINT_DOCUMENTATION' and
+        'DOMAIN' out of app.config as well as app.url_map
+
+    The Hirarchy of Information is:
+    1. list all endpoints from url_map
+    2. add data out of BLUEPRINT_DOCUMENTATION where additional data is given
+    3. update with data out of DOMAIN
+
+    Therefore, if DOMAIN may overrite documentation from
+    BLUEPRINT_DOCUMENTATION, but in general blueprints are not described
+    in DOMAIN, which is why we introduced this extra dict-item.
+
+    :returns: dict with 'base', 'server_name', 'api_name', 'domains'
+    """
     cfg = {}
     base = home_link()['href']
     if '://' not in base:
@@ -35,17 +50,31 @@ def get_cfg():
 
 
 def parse_map(url_map, config):
+    """
+    will extract information out of the url_map and provide them in a dict-form
+    :param url_map: an url_map in the format like app.url_map from eve
+    :param config: the app.config dict from the eve-app the url_map belongs to
+    :returns: empty dict if url-endpoints with methods
+    """
     ret = {}
     for rule in url_map.iter_rules():
-        help = str(rule)
-        resource = help.split("/")[1]
-        path = re.sub(r'<(?:[^>]+:)?([^>]+)>', '{\\1}', help)
+        line = str(rule)
+        # first part if the rule specifies the endpoint
+        # between the first two '/' is the resource
+        resource = line.split("/")[1]
+        # the endpoint is described by a regex, but we want only the name
+        path = re.sub(r'<(?:[^>]+:)?([^>]+)>', '{\\1}', line)
         if resource not in ret:
+            # this is the first path of this resource, create dict-entry
             ret[resource] = {'paths': {}, 'description': {}}
+        # add path to dict
         ret[resource]['paths'][path] = {}
         for method in rule.methods:
             if method in ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']:
+                # we only display these methods, other HTTP-Methods don't need
+                # documentation
                 ret[resource]['paths'][path][method] = {}
+
             doc = config['BLUEPRINT_DOCUMENTATION'].get(resource)
             doc_schema = None
             if doc is not None:
@@ -68,6 +97,13 @@ def identifier(resource):
 
 
 def schema(resource, field=None):
+    """
+    extracts the detailed cerberus-schema of this endpoint
+    :param resource: the resource of the endpoint
+    :param field: the field for which the schema will be returned.
+        If no field specified, return a dict for all fields of the endpoint
+    :returns: schema as dict
+    """
     ret = []
     if field is not None:
         params = {field: resource['schema'][field]}
@@ -99,6 +135,12 @@ def schema(resource, field=None):
 
 
 def endpoint_definition(domain, resource):
+    """
+    gets the documentation of a specified endpoint
+    :param domain: the endpoint
+    :param resource: the resource-subdict of config['DOMAIN']
+    :returns: the documentation as a dict (paths, methods, fields)
+    """
     ret = {}
     ret['description'] = resource['description']
     ret['paths'] = paths(domain, resource)
@@ -106,12 +148,21 @@ def endpoint_definition(domain, resource):
 
 
 def paths(domain, resource):
+    """returns the documentation of all endpoints of a domain for which we have
+    descriptions in the config
+    :param domain: the domain of the endpoints
+    :param resource: the resource-subdict of config['DOMAIN']
+    :returns: dict with paths and their documentation (methods, fields)
+    """
+    # resoruce-endpoint
     ret = {}
     path = '/{0}'.format(resource.get('url', domain))
+    # we don't care about the refex definition, we want the name of the path
     path = re.sub(r'<(?:[^>]+:)?([^>]+)>', '{\\1}', path)
     pathtype = 'resource'
     ret[path] = methods(domain, resource, pathtype)
 
+    # item-endpoint
     primary = identifier(resource)
     path = '{0}/{1}'.format(path, pathparam(primary['name']))
     pathtype = 'item'
@@ -127,8 +178,12 @@ def paths(domain, resource):
 
 def methods(domain, resource, pathtype, param=None):
     """
-    :param domain:
-    :param pathtype: String from ('item', 'resource', ...)
+    extracts mathods and descriptions of a sepcified path
+    :param domain: the domain of the endpoint
+    :param resource: the resource-subdict of config['DOMAIN']
+    :param pathtype: String from ('item', 'resource')
+    :param param:
+    :returns: dict of methods and their documentation (fields)
     """
     ret = {}
     if pathtype == 'additional_lookup':
@@ -158,6 +213,13 @@ def pathparam(param):
 
 
 def get_label(domain, pathtype, method):
+    """
+    a description of what the method does (e.g. PATCH will upadate an item)
+    :param domain: the domain of the endpoint
+    :param pathtype: String from ('item', 'resource')
+    :param method: the method for this label
+    :returns: description as a string
+    """
     verb = LABELS[method]
     if method == 'POST' or pathtype != 'resource':
         noun = capp.config['DOMAIN'][domain]['item_title']
